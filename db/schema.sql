35bb2ed2798a83cb65df5b33ee749fe76e9b7089
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-28 15:10:49 -0400

    chore(table3): create the employee table to store and retrieve the info related to employees

--- db/schema.sql
+++ db/schema.sql
@@ -22,3 +22,18 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE SET NULL
 );
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
