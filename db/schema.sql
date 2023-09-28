5968b6bd06b2a1cc8a7132833a442136b824d83a
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-28 14:40:37 -0400

    chor(table1): create a department table to store related data

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS management_db;
 CREATE DATABASE management_db;
 
 USE management_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
