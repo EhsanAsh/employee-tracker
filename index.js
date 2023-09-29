5f5458d5b2eb7bb85e4498412407a501738109c9
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-09-29 17:24:17 -0400

    feat(init): create a function to initialize the app

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// Description: Main entry point for the application
+// Author: EhsanAsh
+// Last Modified: 2023/10/01
+// ------------------------------------------------
+
+// Dependencies:
 const inquirer = require('inquirer');
-const fs = require('fs');
 const logo = require('asciiart-logo');
 const mysql = require('mysql2');
 const queries = require('./lib/queries');
+const fs = require('fs');
+const { readFile } = require('fs/promises');
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
 
 const connection = mysql.createConnection(
   {
@@ -11,5 +20,25 @@ const connection = mysql.createConnection(
     password: 'Porsche@#911GT3',
     database: 'management_db'
   },
-  console.log(`Connected to the management_db database.`)
+  // console.log(`Connected to the management_db database.`)
 );
+
+connection.connect((err) => err ? console.error(err) : console.log(`Connected to the management_db database.`));
+
+const init = () => { 
+
+  console.log(
+    logo({
+      name: 'Employee Management System',
+      font: 'Big',
+      lineChars: 10,
+      padding: 2,
+      margin: 3,
+      borderColor: 'grey',
+      logoColor: 'bold-green',
+      textColor: 'green',
+    })
+    .render()
+  );
+
+};
